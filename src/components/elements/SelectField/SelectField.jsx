8bfa2a6b569c7b7737be7be7e573ec030402a617
parag441 <67221439+parag441@users.noreply.github.com>
2024-09-13 21:14:19 +0530

    feat: Added react-select library

--- src/components/elements/SelectField/SelectField.jsx
+++ src/components/elements/SelectField/SelectField.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import Select from 'react-select';
 import { dropdownContainer } from './SelectField.module.scss';
+
 function Dropdown() {
+  const options = [
+    { value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
+    { value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
+    { value: 'purple', label: 'Purple', color: '#5243AA' },
+    { value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
+    { value: 'orange', label: 'Orange', color: '#FF8B00' },
+    { value: 'yellow', label: 'Yellow', color: '#FFC400' },
+    { value: 'green', label: 'Green', color: '#36B37E' },
+    { value: 'forest', label: 'Forest', color: '#00875A' },
+    { value: 'slate', label: 'Slate', color: '#253858' },
+    { value: 'silver', label: 'Silver', color: '#666666' },
+  ];
   return (
- 
-      <select className={dropdownContainer}>
-        <option value="">select...</option>
-        <option value="saab">Saab</option>
-        <option value="fiat">Fiat</option>
-        <option value="audi">Audi</option>
-      </select>
-  
+    <Select
+    className="basic-single"
+    classNamePrefix="select"
+    defaultValue={options[0]}
+    isDisabled={false}
+    isLoading={false}
+    isClearable={true}
+    isRtl={false}
+    isSearchable={true}
+    name="color"
+    options={options}
+    /> 
   );
 }
 
--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,110 @@
 export const dummyData = [
   {
+    id: 's0',
+    type: 'dropdown',
+    visited: false,
+    submitted: false,
+    statement: {
+      content: [
+        {
+          component_name: 'html',
+          component_id: 'stmt_id_1',
+          file_name: ['<p>What is the capital of France?</p>'],
+        },
+      ],
+    },
+    //   stem_image: {
+    //     component_name: 'image',
+    //     component_id: 'stmt_id_1',
+    //     file_name: ['https://via.placeholder.com/150'],
+    //   },
+    see_why: {
+      content: [
+        {
+          component_name: 'html',
+          component_id: 'why_id_1',
+          file_name: [
+            '<p>The capital of France is Paris. It is known for its rich history and culture.</p>',
+          ],
+        },
+      ],
+    },
+    options: [
+      {
+        option_id: 1,
+        is_correct: true,
+        content: [
+          {
+            component_name: 'html',
+            component_id: 'opt1_id',
+            file_name: [
+              '<p>Paris- Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s</p>',
+            ],
+          },
+        ],
+      },
+      {
+        option_id: 2,
+        is_correct: false,
+        content: [
+          {
+            component_name: 'html',
+            component_id: 'opt2_id',
+            file_name: [
+              '<p>London -Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s</p>',
+            ],
+          },
+        ],
+      },
+      {
+        option_id: 3,
+        is_correct: false,
+        content: [
+          {
+            component_name: 'html',
+            component_id: 'opt3_id',
+            file_name: [
+              '<p>Berlin-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s</p>',
+            ],
+          },
+        ],
+      },
+      {
+        option_id: 4,
+        is_correct: false,
+        content: [
+          {
+            component_name: 'html',
+            component_id: 'opt4_id',
+            file_name: [
+              '<p>Madrid- Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s</p>',
+            ],
+          },
+        ],
+      },
+    ],
+    feedback: {
+      correct: {
+        content: [
+          {
+            component_name: 'html',
+            component_id: 'correct_fb_id',
+            file_name: ['<p>Correct! Paris is the capital of France.</p>'],
+          },
+        ],
+      },
+      incorrect: {
+        content: [
+          {
+            component_name: 'html',
+            component_id: 'incorrect_fb_id',
+            file_name: ['<p>Incorrect. The correct answer is Paris.</p>'],
+          },
+        ],
+      },
+    },
+  },
+  {
     id: 's1',
     type: 'mcq',
     visited: false,
